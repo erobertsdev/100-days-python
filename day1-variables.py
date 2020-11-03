89cb1403638b0f147b12d75e21138ef2b1363588
Elias Roberts <elias@eroberts.dev>
2020-11-03 07:15:05 -0700

    finished day 1 project

--- day1-variables.py
+++ day1-variables.py
@@ -5,4 +5,18 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
-#String manipulation
+# String manipulation
+print("Hello World!\nNew Line, baybyyyy")
+print("Hello" + " Elias")
+
+# Input
+# name = input("What is your name? ")
+# print(name)
+# length = len(name)
+# print(length)
+
+# Project - Band Name Generator
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in ?\n")
+pet_name = input("What's your pet's name?\n")
+print(f"Your band name could be {city_name} {pet_name}")
