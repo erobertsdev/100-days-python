cdba6ec14525cbfde86ff94fa94f9214a3e2fb81
Elias Roberts <elias@eroberts.dev>
2020-11-03 14:29:38 -0700

    finished day 6 project

---
+++ day6-functions.py
@@ -0,0 +1,15 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# while loop
+# while true
+#   do stuff
+times = 5
+while times > 0:
+    print(times)
+    times -= 1
+
+# Day 6 project - there isn't one kbaiiiiii
