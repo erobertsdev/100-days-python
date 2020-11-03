75ac6d8594dd1f9a76bfb5a658312a8e4324fadf
Elias Roberts <elias@eroberts.dev>
2020-11-03 06:56:55 -0700

    renamed day1

--- day1-variables.py
+++ day1-variables.py
@@ -1,5 +1,8 @@
+# Printing
 print("Hello World")
 
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
-print("print(\'what to print\')")
+print("print('what to print')")
+
+#String manipulation
