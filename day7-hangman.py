a6ee0ac92f1dd8501ab0a85fcb45a7a3ed9560ee
Elias Roberts <elias@eroberts.dev>
2020-11-03 15:21:38 -0700

    finished day 7 project

---
+++ day7-hangman.py
@@ -0,0 +1,33 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel", "dolphin", "beaver", "elephant", "platypus"]
+word = random.choice(word_list)
+display = []
+
+for char in word:
+    display += '_'
+print(display)
+
+guess_count = 0
+lives = 6
+while "_" in display and lives != 0:
+    guess = input("Guess a letter: ").lower()
+
+    if guess not in display:
+        if guess not in word:
+            lives -= 1
+
+        for idx, letter in enumerate(word):
+            if guess == letter:
+                display[idx] = guess
+
+        print(display)
+        print(f"You have {lives} guesses remaining.")
+    else:
+        print(f"You already guessed {guess}, try again.")
+        print(f"You have {lives} guesses remaining.")
+
+if lives > 0:
+    print("You win!")
+else:
+    print(f"You lose! The word was '{word}'.")
