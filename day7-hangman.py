884adada44f199d8296472bad91ba55234f8a07a
Elias Roberts <elias@eroberts.dev>
2020-11-03 15:23:15 -0700

    finished day 7 project

--- day7-hangman.py
+++ day7-hangman.py
@@ -4,10 +4,12 @@ word_list = ["aardvark", "baboon", "camel", "dolphin", "beaver", "elephant", "pl
 word = random.choice(word_list)
 display = []
 
+# create blank word display
 for char in word:
     display += '_'
 print(display)
 
+# the game
 guess_count = 0
 lives = 6
 while "_" in display and lives != 0:
@@ -27,6 +29,7 @@ while "_" in display and lives != 0:
         print(f"You already guessed {guess}, try again.")
         print(f"You have {lives} guesses remaining.")
 
+# game over messages
 if lives > 0:
     print("You win!")
 else:
