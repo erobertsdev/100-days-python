135ec52c67b58337339fb0d9bcfbdac0d1e17f21
Elias Roberts <elias@eroberts.dev>
2020-11-03 10:03:50 -0700

    pi moduuuuuuuuuuuule

---
+++ day4-random-lists.py
@@ -0,0 +1,26 @@
+import random
+import my_module # module with pi variable
+
+# random.seed(1233)
+
+# random_integer = random.randint(1, 10) # random number between 1 and 10 inclusive
+# print(random_integer)
+
+# print(my_module.pi)
+
+# random_float = random.random() * 5
+# print(random_float)
+
+# heads or tails exercise
+# test_seed = int(input("Create a seed number: "))
+# random.seed(test_seed)
+# coin_toss = random.randint(0,1)
+# if coin_toss == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# lists
+
+states = ["Delaware", "Pennsylvania", "New Mexico", "Colorado"]
+print(states[0]) # Delaware
