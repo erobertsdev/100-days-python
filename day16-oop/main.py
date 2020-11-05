25bda8e480cc1fda9a2cac5f22db485780493d01
Elias Roberts <elias@eroberts.dev>
2020-11-05 11:13:29 -0700

    day 16 notes

---
+++ day16-oop/main.py
@@ -0,0 +1,23 @@
+# # import turtle
+# from turtle import Turtle, Screen
+
+# # clementine = turtle.Turtle()
+# clementine = Turtle()
+# clementine.shape("turtle")
+# clementine.color("green")
+# clementine.forward(100)
+
+# print(clementine)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+
+# print(table)
